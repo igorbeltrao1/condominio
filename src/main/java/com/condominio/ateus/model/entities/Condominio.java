04d8017d2b8365d9f517be45a1c5d991782f43b5
igorbeltrao <igorbeltrao96@gmail.com>
2024-07-02 20:00:49 -0300

    initial commit

--- src/main/java/com/condominio/ateus/model/entities/Condominio.java
+++ src/main/java/com/condominio/ateus/model/entities/Condominio.java
@@ -1,17 +1,15 @@
 package com.condominio.ateus.model.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "condominio")
 public class Condominio {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String nome;
